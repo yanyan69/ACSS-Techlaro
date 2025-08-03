05869b7012c9c67591872f75f9d05420d944ec3e
yanyan69 <christiannarvaez300@gmail.com>
2025-08-03 23:46:14 +0800

    update tflite model

--- convert_model.py
+++ convert_model.py
@@ -1,12 +1,12 @@
 # convert_model.py
 import tensorflow as tf
 
-model = tf.keras.models.load_model("copra_classifier/models/copra_model.keras")
-
+model = tf.keras.models.load_model('copra_classifier/models/copra_model.keras')
 converter = tf.lite.TFLiteConverter.from_keras_model(model)
+converter.optimizations = [tf.lite.Optimize.DEFAULT]  # or skip for full precision
 tflite_model = converter.convert()
 
-with open("copra_classifier/models/copra_model.tflite", "wb") as f:
+with open('copra_classifier/models/copra_model.tflite', 'wb') as f:
     f.write(tflite_model)
 
 print("✅ Model converted to TFLite!")

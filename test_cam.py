6ef0a8f4ab0b4dd404089dfc8cc039855e92610b
yanyan69 <christiannarvaez300@gmail.com>
2025-08-12 11:48:18 +0800

    integration of rpi cam image processing into the main UI

--- test_cam.py
+++ test_cam.py
@@ -1,12 +1,15 @@
 import cv2
-cap = cv2.VideoCapture(0)
-while True:
-    ret, frame = cap.read()
-    if not ret:
-        print("Can't receive frame (stream end?). Exiting ...")
-        break
-    cv2.imshow('test', frame)
-    if cv2.waitKey(1) == ord('q'):
-        break
-cap.release()
+
+for i in range(3):  # try 0,1,2
+    cap = cv2.VideoCapture(i)
+    if cap.isOpened():
+        print(f"Camera index {i} opened successfully")
+        ret, frame = cap.read()
+        if ret:
+            print(f"Frame captured from camera {i}")
+            cv2.imshow(f'Camera {i}', frame)
+            cv2.waitKey(1000)
+        cap.release()
+    else:
+        print(f"Camera index {i} failed to open")
 cv2.destroyAllWindows()

41c2c656bd4336979312074bc9aff328ce82da2d
yanyan69 <christiannarvaez300@gmail.com>
2025-10-02 02:27:27 +0800

    added a debug.py to test components

--- servo/servo.py
+++ servo/servo.py
@@ -2,7 +2,7 @@ import serial
 import time
 
 # Open serial port (check /dev/ttyACM0 or /dev/ttyUSB0 on your Pi)
-ser = serial.Serial('/dev/ttyACM0', 9600, timeout=1)
+ser = serial.Serial('/dev/ttyUSB0', 9600, timeout=1)
 time.sleep(2)  # wait for Arduino reset
 
 print("Sending S command to Arduino...")
